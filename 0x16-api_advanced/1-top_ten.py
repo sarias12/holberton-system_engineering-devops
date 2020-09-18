dfea7a283a7b2aabd34217174b1f102cd667b111
sarias12 <1638@holbertonschool.com>
2020-09-18 00:05:11 +0000

    Task 1 - Top Ten hot post about a subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+queries the Reddit API and prints the titles of the first 10 hot posts
+listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot.json?limit=10'
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print('None')
+    else:
+        data = response.json()
+        list_dicts = data.get('data').get('children')
+        for dict in list_dicts:
+            print(dict['data']['title'])
